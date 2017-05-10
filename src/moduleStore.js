2cb93e62d885dc7269af71f3dc76645301dca278
emensch <eli.mensch@gmail.com>
2017-05-09 20:09:55 -0400

    Abstract module storage into specialized object

---
+++ src/moduleStore.js
@@ -0,0 +1,22 @@
+export default () => {
+    const commands = {};
+    const tickers = [];
+
+    return Object.create({
+        addCommand(trigger, description, executeFn) {
+            commands[trigger] = {executeFn, description}
+        },
+
+        addTicker(module) {
+            tickers.push(module)
+        },
+
+        getCommands() {
+            return commands
+        },
+
+        getTickers() {
+            return tickers
+        }
+    })
+}
