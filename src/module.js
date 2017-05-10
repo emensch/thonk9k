7c76680df0f3113d95819a21e3394a41ff7435c6
emensch <eli.mensch@gmail.com>
2017-05-09 20:08:13 -0400

    Create dynamic, compositable module system (wtf am I doin)

--- src/module.js
+++ src/module.js
@@ -1,9 +1,17 @@
-export default module => {
-    const proto = {
-        async load() {
-            return true
+export default (...components) => {
+    let loaders = [];
+
+    components.forEach(elem => {
+        elem.load(loaders)
+    });
+
+    let proto = {
+        load(state) {
+            loaders.forEach(loader => {
+                loader(state)
+            })
         }
     };
 
-    return Object.assign(Object.create(proto), module)
+    return Object.assign(Object.create(proto), ...components)
 }
