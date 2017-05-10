44a98263c807aa2a249c858d8bfd932c0c350240
emensch <eli.mensch@gmail.com>
2017-05-10 04:38:54 -0400

    Add util to "humanize" numbers

---
+++ src/utils/humanize.js
@@ -0,0 +1,12 @@
+export default num => {
+    const strNum = num.toFixed(2).toString();
+    const places = strNum.indexOf('.');
+
+    if (places < 7) {
+        // I absolutely ripped this regex off StackOverflow
+        return strNum.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    const truncated = num / 10 ** (Math.floor((places - 1)/3) * 3);
+    return truncated.toFixed(2) + (places > 9 ? 'b' : 'm');
+}
