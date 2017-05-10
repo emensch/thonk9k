af9bee8cb2b309ef2f47a39d74722f6c363c31d7
emensch <eli.mensch@gmail.com>
2017-05-09 21:32:39 -0400

    Add help command

---
+++ src/core/help/index.js
@@ -0,0 +1,17 @@
+import module from '../../module'
+import command from '../../components/command'
+import loader from '../../components/loader'
+
+
+export default module(
+    command('help', 'Lists all commands alongside their description.', (state, message, args) => {
+        const commands = state.modules.getCommands();
+        let response = '__**Available commands:**__\n';
+
+        Object.keys(commands).forEach( key => {
+            response += `**${state.commandPrefix + key}**: ${commands[key].description}\n`
+        });
+
+        message.channel.sendMessage(response)
+    })
+)
