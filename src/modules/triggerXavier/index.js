e3a3f78ce2668b1ada43246ccc6b77e72e197a00
Eric Schmiedel <eschmiedel@nsareit.net>
2017-09-28 13:09:31 -0600

    added more messages for triggering xavier, and made sure to @ him so he dosn't miss a sinlge one

--- src/modules/triggerXavier/index.js
+++ src/modules/triggerXavier/index.js
@@ -8,14 +8,16 @@ export default module(
                 'funkadelic is shit',
                 'oh that one dude who plays one riff...and was only brought back into the spotlight when....lol....Daft Punk was like HURRR do that same riff for us',
                 'hey niles, play that one riff people know you for',
-                'chromeo is better than niles (who the fuck is niles?)'
+                'chromeo is better than niles (who the fuck is niles?)',
+                'Enyos are only good for one thing: Wrecks',
+                'Bulleit & Woodford are barely worthy of cleaning the oil stains off my driveway'
                 // i guess more stuff that triggers papa X goes here
             ];
 
             const selectedIndex = Math.floor(Math.random() * triggeringMessages.length);
 
             message.channel.send(
-                triggeringMessages[selectedIndex]
+                '@Xavier W#8307 ' + triggeringMessages[selectedIndex]
             );
         } catch (e) {
             throw e;
