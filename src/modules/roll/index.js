d68f7cabf2b44b478c26061d892de59d720645ca
Eli Mensch <eli@Elis-MacBook-Pro.local>
2018-01-24 22:44:56 -0500

    Updated roll syntax

--- src/modules/roll/index.js
+++ src/modules/roll/index.js
@@ -5,17 +5,38 @@ import axios from 'axios'
 export default module(
     command('roll', 'Rolls a die. Optional parameter specifies sides - defaults to 20.', async (state, message, args) => {
         try {
-            const sides = args.split(' ')[0] || 20;
-            const number = Math.floor(Math.random() * sides) + 1;
+            const formatted = args.split(' ')[0] || '1d20';
+            const parsed = formatted.split('d');
+            const number = parsed[0];
+            const sides = parsed[1];
+            let messageToSend = '';
 
-            if(isNaN(sides)) {
+            if(isNaN(sides) || isNaN(number)) {
                 message.channel.send('That ain\'t a number, dumbass.')
-            } else if (sides < 2) {
+            } else if (sides < 2 || number < 1) {
                 message.channel.send('That\'s impossible.')
+            } else if (number > 20 || parsed.length > 3) {
+                message.channel.send('Don\'t be stupid.')
             } else {
-                message.channel.send(
-                    `You rolled a d${sides} and got **${number}**.`
-                );
+                let results = [];
+
+                for(let i = 0; i < number; i++) {
+                    results.push(Math.floor(Math.random() * sides) + 1)
+                }
+
+                if(parseInt(number) === 1) {
+                    messageToSend = `You rolled ${number}d${sides} and got **${results[0]}**.`
+                } else {
+                    let messagePart = '';
+                    let total = results.reduce((acc, val) => acc + val);
+                    results.forEach((e, idx) => {
+                        messagePart += (idx > 0) ? `, **${e}**` : `**${e}**`
+                    });
+
+                    messageToSend = `You rolled ${number}d${sides} and got: ${messagePart} for a total of **${total}**.`
+                }
+
+                message.channel.send(messageToSend)
             }
 
         } catch(e) {
