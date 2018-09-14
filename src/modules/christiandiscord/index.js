5306a6f79a4e942365674487a0fb142cb11a3fd5
Eli Mensch <eli@Elis-MacBook-Pro.local>
2018-09-13 21:52:34 -0400

    Whoops bb

--- src/modules/christiandiscord/index.js
+++ src/modules/christiandiscord/index.js
@@ -1,3 +1,17 @@
-/**
- * Created by eli on 9/13/18.
- */
+import module from '../../module'
+import command from '../../components/command'
+import loader from '../../components/loader'
+import axios from 'axios'
+
+export default module(
+    command('christiandiscord', 'Maintains our status as a good Christian discord.', async (state, message, args) => {
+        try {
+            const { data: [passage] } = await axios.get(`http://labs.bible.org/api/?passage=random&type=json`);
+            const { bookname, chapter, verse, text } = passage;
+
+            message.channel.send(`**${bookname} ${chapter}:${verse}** ${text}`)
+        } catch(e) {
+            throw e
+        }
+    })
+)
